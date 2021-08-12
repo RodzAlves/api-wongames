dd5b790da5d1e40cd3c17e02e5c3bc02ce4c696e
Rodrigo Alves <rodrigoalvesbrasileiro@gmail.com>
2021-08-12 19:16:59 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '82942b65638521e159e3307801a8807f'),
+    },
+  },
+});
